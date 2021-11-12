00f92c12fdba0cf80b93d476d2d6f0a8e883b74c
volkannarinc <volkannarincc@gmail.com>
2021-11-13 02:17:26 +0300

    NRNC-6 : Kotlin Detekt

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -8,9 +8,11 @@ object Config {
 
     object Plugins {
         const val kotlin = "kotlin"
-        const val android = "com.android.application"
+        const val androidApplication = "com.android.application"
+        const val androidLibrary = "com.android.library"
         const val kotlinAndroid = "android"
         const val ktLint = "org.jlleitschuh.gradle.ktlint"
+        const val detekt = "io.gitlab.arturbosch.detekt"
     }
 
     const val testRunner = "androidx.test.runner.AndroidJUnitRunner"
--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -4,6 +4,7 @@ object Versions {
     const val kotlinVersion = "1.5.20"
     const val ktLintVersion = "10.2.0"
     const val ktLintSnapshotVersion = "0.43.0"
+    const val detektVersion = "1.19.0-RC1"
 
     // Core dependencies
     const val coreKtxVersion = "1.7.0"
