3ffb0b46c45e1c23e797822c06dc063f5f4d94b4
volkannarinc <volkannarincc@gmail.com>
2021-12-14 02:47:03 +0300

    NRNC-30 : Module : Cache  - removed unnecessary automatic added kotlin plugin code

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,11 +14,6 @@ allprojects {
         mavenCentral()
     }
 }
-buildscript {
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
-    }
-}
 
 subprojects {
     apply {
